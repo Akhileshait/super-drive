7030cd6b77bf6e91ad385234559d179d8bcef8b8
Akhileshait <akhileshpatidar989368@gmail.com>
2024-07-15 01:07:10 +0530

    account updating routers

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser, loginUser, logoutUser, refreshAccessToken} from "../controllers/user.controller.js"
+import {registerUser, loginUser, logoutUser, refreshAccessToken, changepassword, getCurrentUser, updateAccountDetails, updateAvatar, updateCover} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import { VerifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -18,5 +18,20 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(VerifyJWT, logoutUser);
 
 router.route("/refresh").post(refreshAccessToken);
+router.route("/change-pass").post(changepassword);
+router.route("/getuser").post(getCurrentUser);
+router.route("/edit-details").post(updateAccountDetails);
+
+router.route("/avatar-update").post(upload.fields([{
+     name: "avatar",
+     maxCount: 1
+}]),updateAvatar)
+
+router.route("/cover-update").post(upload.fields([{
+     name: "cover",
+     maxCount: 1
+}]),updateCover)
+
+
 
 export default router;
